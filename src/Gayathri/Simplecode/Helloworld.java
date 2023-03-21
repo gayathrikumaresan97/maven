cd39b6d02906bb4b64e3c6462665e8606d2ae5e9
Gayathri Kumaresan <gayathrikumaresan422@gmail.com>
2023-03-21 21:31:01 +0530

    Hello code

---
+++ src/Gayathri/Simplecode/Helloworld.java
@@ -0,0 +1,10 @@
+package Gayathri.Simplecode;
+import java.io.*;
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
